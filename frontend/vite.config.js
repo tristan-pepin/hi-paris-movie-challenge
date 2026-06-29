13a7d5452c2bf80a33ad2c96d49fd02239db44e8
tristan-pepin <peptristan@gmail.com>
2026-06-29 11:32:31 +0200

    feat: front dependencies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': 'http://localhost:8000',
